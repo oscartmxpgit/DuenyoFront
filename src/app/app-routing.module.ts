5078a38cec61732a55d13c973dde8b03e266b8f6
racha.balegroune@gmail.com <racha.balegroune@gmail.com>
2022-04-27 14:00:52 +0200

    Crear negocios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NegocioComponent } from './negocio/negocio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'negocio',
+    component: NegocioComponent,
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { NegocioComponent } from './negocio/negocio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NegocioComponent
   ],
   imports: [
     BrowserModule,

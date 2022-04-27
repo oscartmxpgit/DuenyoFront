d01b34e25d78626affc7553ae3ed7b48bc172c3e
racha.balegroune@gmail.com <racha.balegroune@gmail.com>
2022-04-27 20:17:52 +0200

    anadir los negocios

--- src/app/negocio/negocio.component.ts
+++ src/app/negocio/negocio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NegocioService } from '../negocio.service';
 
 @Component({
   selector: 'app-negocio',
@@ -6,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./negocio.component.sass']
 })
 export class NegocioComponent implements OnInit {
+  public negocios : Array<any>;
 
-  constructor() { }
+  constructor(private negocioService: NegocioService, private router: Router) { 
+    this.negocios = negocioService.getNegocio();
+    console.log(this.negocios)
+  }
+
+ 
+ 
 
   ngOnInit(): void {
   }
